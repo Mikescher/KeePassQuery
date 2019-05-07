4acb84258ce5a5c7424bd95f3c9a29db5ecafb6d
Mike Schwörer <mailport@mikescher.de>
2019-05-07 16:58:50 +0200

    KP Plugin Template

---
+++ Source/KeePassQuery/KeePassQueryExt.cs
@@ -0,0 +1,24 @@
+﻿using KeePass.Plugins;
+
+namespace KeePassQuery
+{
+	public class KeePassQueryExt : Plugin
+	{
+		//public override string UpdateUrl => ""; //TODO
+
+		private IPluginHost _pluginHost;
+
+		public override bool Initialize(IPluginHost host)
+		{
+			if (host == null) return false;
+			_pluginHost = host;
+
+			return true;
+		}
+
+		public override void Terminate()
+		{
+			base.Terminate();
+		}
+	}
+}
---
+++ Source/KeePassQuery/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KeePassQuery")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KeePass Plugin")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("26d7ddac-7c2b-4537-a025-77a295a3a2d9")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
